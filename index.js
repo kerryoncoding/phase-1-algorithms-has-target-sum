cbc1281214db8819b3f8f7a7c917be88d099720a
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-21 17:37:35 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // filter array for elements less than target
+  //const filteredArr = array.filter(e => e<target ) ---> this didn't work with a negative number
+  let findNumber = 0
+  for (let i=0; i<array.length; i++){
+    findNumber = target - array[i]
+    for (let j=i+1; j<array.length; j++){
+      if (findNumber === array[j]) {
+        return true
+      }
+    }
+  } return false
+
+  // walk through filtered array... 
+  // loop through array
+  // let findnumber = element - target
+  // search for number in array
+
+
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  filter array 1
+  stepthroug n
+
 */
 
 /* 
   Add your pseudocode here
+  filter array for elements less than target
+  walk through filtered array... 
+  loop through array
+  let findnumber = element - target
+  search for number in array
+
 */
 
 /*
   Add written explanation of your solution here
+
+  if 2 integers in array sum to be the target, return true 
+  else return false
 */
 
 // You can run `node index.js` to view these console logs
